e72e5f56e964dbd8813cdaee5b9c114640bb63e3
Dustin <dustin.hanusch@mailbox.tu-dresden.de>
2023-01-01 18:15:39 +0100

    4.2

--- Exercise_2_MATLAB/train_reconstruction_net.m
+++ Exercise_2_MATLAB/train_reconstruction_net.m
@@ -12,8 +12,21 @@ load("DATA_MMF_16.mat")
 
 
 %% Create Neural Network Layergraph MLP
-% Layers = [];
 
+inputDim = size(XTest); %Dimension des Input
+outputDim = size(YTest); %Dimensionen Output
+I_px = inputDim(1);
+O_px = outputDim(1);
+
+layers = [
+    imageInputLayer([I_px I_px 1],'Name','Input')
+fullyConnectedLayer(I_px^2,'Name','Fc1')
+reluLayer('Name','Relu1')
+fullyConnectedLayer(O_px^2,'Name','Fc2')
+reluLayer('Name','Relu2')
+depthToSpace2dLayer([O_px O_px],'Name','dts1')
+regressionLayer('Name','Output')
+];
 %% Training network
 % define "trainingOptions"
 % training using "trainNetwork"
